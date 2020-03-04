8524c12a94106da617776ed09fb8830dfa555709
Russell Kurak <rkurak@acumensolutions.com>
2020-03-04 14:42:06 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  var time    = hours+':'+minutes+':'+seconds;
+  return time;
+}
+
+app.get('/', (req, res) => res.send('Hello World! ' + (process.uptime() + "").toHHMMSS()))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
